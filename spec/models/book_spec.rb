958295962c8c842f6f3d86e90804787bb40754fb
DJ <dtaylor88@gmail.com>
2021-11-29 12:33:32 -0800

    add user and book relation to book model

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -7,4 +7,5 @@ RSpec.describe Book, type: :model do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:author) }
   it { should validate_length_of(:title).is_at_least(3) }
+  it { should belong_to(:user) }
 end
