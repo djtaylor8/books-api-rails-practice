4e2014be23a296c56b7be9b7d42c3b1ac5b746df
DJ <dtaylor88@gmail.com>
2021-11-29 12:27:02 -0800

    add user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:username) }
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_length_of_(:username).is_at_least(3) }
+  it { should validate_presence_of(:password) }
+  it { should validate_length_of(:password).is_at_least(5) }
+
+  describe 'Associations' do 
+    it { should have_many(:books) }
+  end
 end
