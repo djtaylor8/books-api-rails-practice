daefef4a621d7d311241ced55555ef3d1d3c03b9
DJ <dtaylor88@gmail.com>
2021-11-29 10:08:01 -0800

    change rails_helper config for json helper method

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,6 +41,15 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+# [...]
+RSpec.configuration do |config|
+  # [...]
+  config.include RequestSpecHelper, type: :request
+  # [...]
+end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -49,9 +58,6 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
-  config.include RequestSpecHelper, type: :request
-
 
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
