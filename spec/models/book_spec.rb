f12841ab93809c012fc36ae7d58134fbafe2a200
DJ <dtaylor88@gmail.com>
2021-11-29 09:52:57 -0800

    add relations to models, pass all initial tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   #Association test
-  it { should belong_to(:catgegory) }
+  it { should belong_to(:category) }
   #Validation tests
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:author) }
